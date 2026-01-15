ac712f718d6d5760f4d34decf4df31a6ec5bb29d
DipjyotiTCS <dipjyoti.som@tcs.com>
2026-01-15 08:42:27 +0530

    US-1-New_API_for_getting_userdetails

--- src/main/java/com/example/usermanagement/dto/UserResponse.java
+++ src/main/java/com/example/usermanagement/dto/UserResponse.java
@@ -1,10 +1,16 @@
 package com.example.usermanagement.dto;
 
+import com.example.usermanagement.entity.User;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserResponse {
     private Long id;
     private String username;
@@ -13,3 +19,14 @@ public class UserResponse {
     private String designation;
     private String organization;
 }
+
+public UserResponse(User user) {
+    this.id = user.getId();
+    this.username = user.getUsername();
+    this.email = user.getEmail();
+    this.fullName = user.getFullName();
+    this.designation = user.getDesignation().getTitle();
+    this.organization = user.getOrganization().getName();
+}
+
+
